924a06948afa56f3bbbc4496f2dfd15541568354
yq-ni <yq.ni@qq.com>
2017-05-19 22:10:15 +0800

    refactor Score to try some pattern

--- src/main/java/cn/cantonese/segmenter/evaluation/Score.java
+++ src/main/java/cn/cantonese/segmenter/evaluation/Score.java
@@ -8,47 +8,43 @@ import cn.cantonese.segmenter.data.SafeDataIterator;
 import java.util.*;
 
 public class Score {
+    private List<Segmenter> segmenters;
+    private List<Data<String>> dataList;
+    private List<Evaluation> evaluations;
 
-    public Score() {
-
+    public Score(ScoreBuilder scoreBuilder) {
+        this.segmenters = scoreBuilder.segmenters;
+        this.dataList = scoreBuilder.dataList;
+        this.evaluations = scoreBuilder.evaluations;
     }
 
-    public void performanceRun(List<Segmenter> segmenters, Data<String> data, int time) {
+    public static ScoreBuilder builder() {
+        return new ScoreBuilder();
+    }
+
+    // TODO: 2017/5/19 calculate data size?
+    public Map<String, Double> performanceRun(int time) {
+        Map<String, Double> timeMap = new HashMap<>();
         for (Segmenter segmenter : segmenters) {
-            long total = performanceRun(segmenter, data, time);
-            System.out.printf("segmenter: %s, cost: %f", segmenter, total);
-        }
-    }
-
-    public long performanceRun(Segmenter segmenter, Data<String> data, int time) {
-        long total = 0;
-        for (int i = 0; i < time; i++) {
-
-            SafeDataIterator<String> iterator = data.safeDataIterator();
-            String test;
-            long start = System.currentTimeMillis();
-            while (iterator.hasNext()) {
-                test = iterator.next();
-                segmenter.processSentence(test);
+            double total = 0;
+            for (int i = 0; i < time; i++) {
+                for (Data<String> data : dataList) {
+                    SafeDataIterator<String> iterator = data.safeDataIterator();
+                    String test;
+                    long start = System.currentTimeMillis();
+                    while (iterator.hasNext()) {
+                        test = iterator.next();
+                        segmenter.processSentence(test);
+                    }
+                    total += System.currentTimeMillis() - start;
+                }
             }
-            total += System.currentTimeMillis() - start;
+            timeMap.put(segmenter.getId(), total / time);
         }
-        return total / time;
+        return timeMap;
     }
 
-    public List<Statistics> calStatistics(Segmenter segmenter, Data<String> data) {
-        return calStatistics(segmenter, data, Arrays.asList(new DefaultEvaluation(), new BMES(), new SentencePerfect()));
-    }
-
-    public List<Statistics> calStatistics(Segmenter segmenter, Data<String> data, List<Evaluation> evaluations) {
-        return calStatistics(Arrays.asList(segmenter), data, evaluations).get(segmenter.getId());
-    }
-
-    public Statistics calStatistics(Segmenter segmenter, Data<String> data, Evaluation evaluation) {
-        return calStatistics(Arrays.asList(segmenter), data, Arrays.asList(evaluation)).get(segmenter.getId()).get(0);
-    }
-
-    public Map<String, List<Statistics>> calStatistics(List<Segmenter> segmenters, Data<String> data, List<Evaluation> evaluations) {
+    public Map<String, List<Statistics>> calStatistics() {
         List<Statistics> statisticsList;
         Map<String, List<Statistics>> map = new HashMap<>();
         for (Segmenter segmenter : segmenters) {
@@ -58,32 +54,60 @@ public class Score {
         }
         String gold, test;
         Statistics tempStatistics;
-        try (DataIterator<String> iterator = data.dataIterator()) {
-            while (iterator.hasNext()) {
-                gold = iterator.next();
-                test = Utils.goldToSource(gold);
-                for (Segmenter segmenter : segmenters) {
-                    String segTest = Utils.tokensToGold(segmenter.processSentence(test));
-                    statisticsList = map.get(segmenter.getId());
-                    for (int i = 0; i < statisticsList.size(); i++) {
-                        tempStatistics = evaluations.get(i).calStatistics(gold, segTest);
-                        if (statisticsList.get(i) == null) {
-                            statisticsList.set(i, tempStatistics);
-                        } else {
-                            statisticsList.get(i).merge(tempStatistics);
+        for (Data<String> data : dataList) {
+            try (DataIterator<String> iterator = data.dataIterator()) {
+                while (iterator.hasNext()) {
+                    gold = iterator.next();
+                    test = Utils.goldToSource(gold);
+                    for (Segmenter segmenter : segmenters) {
+                        String segTest = Utils.tokensToGold(segmenter.processSentence(test));
+                        statisticsList = map.get(segmenter.getId());
+                        for (int i = 0; i < statisticsList.size(); i++) {
+                            tempStatistics = evaluations.get(i).calStatistics(gold, segTest);
+                            if (statisticsList.get(i) == null) {
+                                statisticsList.set(i, tempStatistics);
+                            } else {
+                                statisticsList.get(i).merge(tempStatistics);
+                            }
                         }
                     }
-                }
 
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         return map;
     }
 
-    private interface ProcessTwo {
-        void process(String gold, String test);
-    }
+    public static class ScoreBuilder {
+        private List<Segmenter> segmenters;
+        private List<Data<String>> dataList;
+        private List<Evaluation> evaluations;
 
+        public ScoreBuilder() {
+            segmenters = new ArrayList<>();
+            dataList = new ArrayList<>();
+            evaluations = new ArrayList<>();
+        }
+
+        public ScoreBuilder withSegmenter(Segmenter segmenter) {
+            segmenters.add(segmenter);
+            return this;
+        }
+
+        public ScoreBuilder withData(Data<String> data) {
+            dataList.add(data);
+            return this;
+        }
+
+        public ScoreBuilder withEvaluation(Evaluation evaluation) {
+            evaluations.add(evaluation);
+            return this;
+        }
+
+        public Score build() {
+            return new Score(this);
+        }
+    }
 }
